28a387f0bc6d5b3e3151d361aa35cf2db28bf5bf
João Matheus Gonçalves dos Santos <joaomatheus.contato@hotmail.com>
2023-03-12 14:04:03 -0300

    ajustes nos testes

--- src/tests/Application.UnitTests/Commands/CalcularCdbCommandTest.cs
+++ src/tests/Application.UnitTests/Commands/CalcularCdbCommandTest.cs
@@ -6,6 +6,7 @@ using SimuladorCDB.Application;
 using SimuladorCDB.Application.Commands.CalcularPrevisaoCdb;
 using SimuladorCDB.Application.Handlers.CalcularPrevisaoCdb;
 using SimuladorCDB.Domain.Entities;
+using SimuladorCDB.Domain.Exceptions;
 using SimuladorCDB.Domain.Services;
 
 namespace Application.UnitTests.Commands
@@ -27,24 +28,53 @@ namespace Application.UnitTests.Commands
 
         [Fact]
         [Trait(nameof(CalcularPrevisaoCdbCommand), "Success")]
-        public async Task Enviado_CalcularPrevisaoCdbCommand_Sem_Mapper_Deve_Retornar_ArgumentNullException()
+        public void Enviado_CalcularPrevisaoCdbCommand_SemAgumentoService_Deve_Retornar_ArgumentNullException()
         {
             //Arrange
-            var command = new CalcularPrevisaoCdbCommand(1000, 2);
-            var handler = new CalcularPrevisaoCdbCommandHandler(_calcularCdb, _mapper);
 
             //Act
-            var response = await handler.Handle(command, It.IsAny<CancellationToken>());
 
             //Assert
-            Assert.Multiple(() =>
-            {
-                response.ValorTotalInvestimentoBruto.Should().Be(1019.5344783999999);
-                response.ValorRendimentoBruto.Should().Be(19.534478399999898);
-                response.ValorIRDebitar.Should().Be(4.395257639999977);
-                response.ValorTotalInvestimentoLiquido.Should().Be(1015.13922076);
-                response.ValorRendimentoLiquido.Should().Be(15.139220759999944);
-            });
+            var error = Assert.ThrowsAny<ArgumentNullException>(
+               () => new CalcularPrevisaoCdbCommandHandler(null, _mapper));
+
+            //Assert
+            Assert.IsType<ArgumentNullException>(error);
+            Assert.Equal("calcularCdbService", error.ParamName);
+        }
+
+        [Fact]
+        [Trait(nameof(CalcularPrevisaoCdbCommand), "Success")]
+        public void Enviado_CalcularPrevisaoCdbCommand_SemAgumentoMapper_Deve_Retornar_ArgumentNullException()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            var error = Assert.ThrowsAny<ArgumentNullException>(
+               () => new CalcularPrevisaoCdbCommandHandler(_calcularCdb, null));
+
+            //Assert
+            Assert.IsType<ArgumentNullException>(error);
+            Assert.Equal("mapper", error.ParamName);
+        }
+
+        [Fact]
+        [Trait(nameof(CalcularPrevisaoCdbCommand), "Success")]
+        public async Task Enviado_CalcularPrevisaoCdbCommand_SemRequestArgumento_Deve_Retornar_ArgumentNullException()
+        {
+            //Arrange
+
+            //Act
+            var handler = new CalcularPrevisaoCdbCommandHandler(_calcularCdb, _mapper);
+
+            //Assert
+            var error = await Assert.ThrowsAnyAsync<ArgumentNullException>(
+               () => handler.Handle(null, It.IsAny<CancellationToken>()));
+           
+            Assert.IsType<ArgumentNullException>(error);
+            Assert.Equal("request", error.ParamName);
         }
 
         [Fact]
